b317dde31c3ef6523f6465aeb08827211ec038d7
Иван <68641750+Mister-42@users.noreply.github.com>
2026-06-11 17:16:12 +0200

    German translation

---
+++ messages/de/yii3-recaptcha.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+/** @see \Rasuvaeff\Yii3Recaptcha\RecaptchaV2Rule */
+/** @see \Rasuvaeff\Yii3Recaptcha\RecaptchaV3Rule */
+return [
+    'The CAPTCHA verification failed.' => 'Die CAPTCHA-Überprüfung ist fehlgeschlagen.',
+    'The CAPTCHA score is too low.' => 'Der CAPTCHA-Score ist zu niedrig.',
+    'The CAPTCHA action does not match.' => 'Die CAPTCHA-Aktion stimmt nicht überein.',
+];
